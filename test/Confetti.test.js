32ee720eb1b4aa1ce770cdcc559b5b39c633b92f
Mitchel <mitchel_jager@hotmail.com>
2022-06-01 22:11:35 +0200

    Add basic tests

--- test/Confetti.test.js
+++ test/Confetti.test.js
@@ -1,6 +1,28 @@
 import "jsdom-global/register"
-import { fireEvent, render } from "@testing-library/svelte"
+import { render } from "@testing-library/svelte"
+import Confetti from '../src/Confetti.svelte'
 
-test("If it is true", () => {
-  expect(true).toBeTruthy()
+test("Default Confetti should render with 50 confetti elements", () => {
+  render(Confetti)
+
+  expect(document.querySelectorAll(".confetti").length).toBe(50)
+})
+
+test("If amount is given render that amount of confetti elements", () => {
+  const amount = 42
+  render(Confetti, { amount: amount })
+
+  expect(document.querySelectorAll(".confetti").length).toBe(amount)
+})
+
+test("If rounded prop is given wrapper element has rounded class", () => {
+  render(Confetti, { rounded: true })
+
+  expect(document.querySelector(".rounded")).toBeTruthy()
+})
+
+test("If cone prop is given wrapper element has rounded class", () => {
+  render(Confetti, { cone: true })
+
+  expect(document.querySelector(".cone")).toBeTruthy()
 })
